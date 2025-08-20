e12b07acb3db866ead61ab1419042adfdfaaf04c
Léonard Jouve <leonard.jouve@gmail.com>
2025-08-20 11:01:05 +0200

    lint

--- src/server/wave_function_collapse/index.ts
+++ src/server/wave_function_collapse/index.ts
@@ -104,7 +104,6 @@ export default class WaveFunctionCollapse {
                         stack.push(validNeighborIndex);
                     }
                 }
-
             });
         }
     }
