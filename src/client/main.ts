97ea04a2ba77c32b8b4e9c640d7b41c1ba89b225
Christophe KUNZLI <christophe.kunzli@heig-vd.ch>
2025-08-26 19:33:41 +0200

    Test displaying items

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,4 +1,4 @@
-import {Application, Assets, Container, Sprite} from "pixi.js";
+import {Application} from "pixi.js";
 import Parser from "codebotsinterpreter";
 import {getSpritesheets} from "./spritesheet_atlas";
 import {Interface} from "./interface/interfaces";
@@ -16,8 +16,31 @@ const run = async () => {
     const spritesheets = await getSpritesheets();
 
     const interfaceInstance = new Interface(app, spritesheets, scale);
-    interfaceInstance.drawChestInventory([]);
-    interfaceInstance.drawItemBar([]);
+
+    interfaceInstance.drawChestInventory([
+        {spriteName: "pickaxe", quantity: 1},
+        {spriteName: "shovel", quantity: 1},
+        {spriteName: "axe", quantity: 1},
+        null,
+        {spriteName: "stone", quantity: 64},
+        {spriteName: "coal", quantity: 128},
+        {spriteName: "copper", quantity: 32},
+        null,
+        null,
+        null,
+        {spriteName: "iron", quantity: 9999},
+        null,
+        {spriteName: "iron_frame", quantity: 5},
+        {spriteName: "iron_plate", quantity: 3},
+    ]);
+    interfaceInstance.drawItemBar([
+        {spriteName: "pickaxe", quantity: 1},
+        {spriteName: "shovel", quantity: 1},
+        null,
+        {spriteName: "stone", quantity: 64},
+        null,
+        null,
+    ]);
 }
 
 run();
