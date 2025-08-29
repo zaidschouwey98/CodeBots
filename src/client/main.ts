b971ff65dd42f5f720a49f4be7f9544341f16292
Christophe KUNZLI <christophe.kunzli@heig-vd.ch>
2025-08-29 16:10:36 +0200

    finish merge

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,6 +1,54 @@
-import { Application} from 'pixi.js';
+import {Application} from 'pixi.js';
 
-import { GameEngine } from './game_engine';
+import {GameEngine} from './game_engine';
+import {CoreStep, Recipe} from "./items/item";
+import {getSpritesheets} from "./spritesheet_atlas";
+import {Interface} from "./interface/interfaces";
+
+const exampleRecipes: Recipe[] = [
+    {inputs: [{spriteName: "wood_log", quantity: 1}], output: {spriteName: "wood_plank", quantity: 4}},
+    {inputs: [{spriteName: "iron_ingot", quantity: 1}], output: {spriteName: "nail", quantity: 16}},
+    {
+        inputs: [{spriteName: "wood_plank", quantity: 12}, {spriteName: "nail", quantity: 64}],
+        output: {spriteName: "crate", quantity: 1}
+    },
+    {
+        inputs: [{spriteName: "stone", quantity: 8}, {spriteName: "coal", quantity: 2}, {
+            spriteName: "iron_ore",
+            quantity: 1
+        }], output: {spriteName: "furnace_off", quantity: 1}
+    },
+    {
+        inputs: [{spriteName: "wood_plank", quantity: 3}, {spriteName: "iron_rod", quantity: 2}, {
+            spriteName: "nail",
+            quantity: 16
+        }], output: {spriteName: "pickaxe", quantity: 1}
+    },
+    {
+        inputs: [{spriteName: "wood_plank", quantity: 3}, {spriteName: "iron_rod", quantity: 2}, {
+            spriteName: "nail",
+            quantity: 8
+        }], output: {spriteName: "shovel", quantity: 1}
+    },
+    {
+        inputs: [{spriteName: "wood_plank", quantity: 3}, {spriteName: "iron_rod", quantity: 2}, {
+            spriteName: "nail",
+            quantity: 16
+        }], output: {spriteName: "axe", quantity: 1}
+    },
+];
+
+const exampleCoreStep: CoreStep = {
+    stepNumber: 1,
+    items: [
+        {spriteName: "wood_plank", currentGathered: 0, goal: 20},
+        {spriteName: "stone", currentGathered: 0, goal: 10},
+        {spriteName: "coal", currentGathered: 0, goal: 5},
+        {spriteName: "iron_ore", currentGathered: 0, goal: 5},
+        {spriteName: "iron_ingot", currentGathered: 0, goal: 5},
+        {spriteName: "nail", currentGathered: 0, goal: 50}
+    ]
+};
 
 (async () => {
     // Create a new application
@@ -23,6 +71,19 @@ import { GameEngine } from './game_engine';
         engine.update(delta.deltaTime);
     });
 
+    const spritesheets = await getSpritesheets();
+    const gui = new Interface(app, spritesheets, scale);
 
+    gui.drawCraftingInterface(exampleRecipes);
+//gui.drawCoreInterface(exampleCoreStep);
+
+    gui.drawItemBar([
+        {spriteName: "pickaxe", quantity: 1},
+        {spriteName: "shovel", quantity: 1},
+        null,
+        {spriteName: "stone", quantity: 64},
+        null,
+        null,
+    ]);
 
 })();
