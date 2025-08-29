1657b8033659195682a1a1e63d35a5544c26fbe4
Léonard Jouve <leonard.jouve@gmail.com>
2025-08-29 08:45:11 +0200

    fix performance issue

--- src/client/constants.ts
+++ src/client/constants.ts
@@ -1,5 +1,5 @@
-export const CHUNK_SIZE = 16;
+export const CHUNK_SIZE = 8;
 export const TILE_SIZE = 16;
-export const RENDER_DISTANCE = 2;
-export const CHUNK_LOAD_RADIUS = 2;
+export const RENDER_DISTANCE = 4;
+export const CHUNK_LOAD_RADIUS = 4;
 export const ANIMATION_SPEED = 0.1;
